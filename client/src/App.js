27daa5a54afd0c2d38aa6780f95a6282f2502b4c
Pietro Coelho <ppereira@atlassian.com>
2019-03-13 16:20:21 +0100

    change title

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+            <h1 className="App-title">Fib calculator</h1>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
